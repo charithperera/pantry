b9dd51b2c8c59c5573a82fe0fa59ce79fc7f30f7
charithperera <charith.p@gmail.com>
2016-11-04 01:31:33 +1100

    Deploy Ready

--- db_config.rb
+++ db_config.rb
@@ -5,4 +5,5 @@ options = {
   database: 'pantry'
 }
 
-ActiveRecord::Base.establish_connection(options)
+# ActiveRecord::Base.establish_connection(options)
+ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || options)
